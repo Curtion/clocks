13d436f6bf4a886d1403ba5f10c29c8e37e37d67
Curtion <curtion@126.com>
2025-12-22 12:18:14 +0800

    feat: 新增一些模型

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 AI时钟, 使用各种模型。
 
+由于AI具有随机性, 当前效果不能完全代表模型强度, 仅供参考。
+
 # 参考
 
 https://clocks.brianmoore.com/
--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.4" .
-docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.4"
+docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.5" .
+docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.5"
