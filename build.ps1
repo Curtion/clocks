5fa2c3d189eb67b288cd847dd108c5624a96fa1a
Curtion <curtion@126.com>
2025-12-24 16:29:43 +0800

    chore: 更新 Docker 镜像标签至 1.0.7

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.6" .
-docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.6"
+docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.7" .
+docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.7"
