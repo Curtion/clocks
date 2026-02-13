60e56eb5bce130bdbc8afc4a42dcbd113f202561
Curtion <curtion@126.com>
2026-02-13 09:45:18 +0800

    feat: 更新 Docker 镜像标签至 1.0.12，并新增模拟时钟页面

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.11" .
-docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.11"
+docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.12" .
+docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.12"
