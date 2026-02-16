508cc7c2c70cb0e9aa1fc0f3aa607d2f61580bb8
Curtion <curtion@126.com>
2026-02-16 23:46:15 +0800

    feat: 新增qwen3.5

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.12" .
-docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.12"
+docker build -t "ccr.ccs.tencentyun.com/curtion/clocks:1.0.13" .
+docker push "ccr.ccs.tencentyun.com/curtion/clocks:1.0.13"
